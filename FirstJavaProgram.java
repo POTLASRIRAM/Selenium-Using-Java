00f9118f218f5899d265b42739b01f7ff1d3aab8
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2023-10-01 23:43:10 -0500

    Add files via upload

---
+++ FirstJavaProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package day1;
+
+/**
+ * 
+ */
+public class FirstJavaProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hiii");
+
+	}
+
+}
