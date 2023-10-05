9ba0ec73122adecc8610624ddbc9cdd7c2fa7af5
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2023-10-05 00:01:27 -0500

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=200;
+		
+		a=a*b;
+		b=a/b;
+		a=a/b;
+		
+		System.out.println("The value of a :"+a);
+		System.out.println("The value of b :"+b);
+	
+
+	}
+
+}
